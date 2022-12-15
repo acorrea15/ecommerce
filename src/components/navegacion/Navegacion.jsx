5f4d9e53fe9a04ead91277bedadc0cf6e1a0d1e1
adolfo85 <adolfodeboeck@gmail.com>
2022-12-15 11:05:10 -0300

    primer formato de nav

--- src/components/navegacion/Navegacion.jsx
+++ src/components/navegacion/Navegacion.jsx
@@ -26,7 +26,7 @@ export const Navegador = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="home">Home</Nav.Link>
-              <Nav.Link href="dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="dashboard">Sobre nosotros</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
