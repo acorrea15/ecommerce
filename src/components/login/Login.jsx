63fd21304aff515e0a531e5b79d53ba2067c0fc0
adolfo85 <adolfodeboeck@gmail.com>
2022-12-16 11:27:59 -0300

    agregado de algunos comentarios en la lógica del logins

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,9 +3,13 @@ import { Form, Button, Modal } from "react-bootstrap";
 import Swal from "sweetalert2";
 
 const Login = () => {
+  // El useState nos da el estado inicial de los parámetros.  
+  // El estado inicial es un string vacío ("")
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+// La función handlesubmit sirve para enviar los datos del formulario. 
+// Recibimos un evento y le pasamos la lógica.
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
@@ -26,12 +30,16 @@ const Login = () => {
 
   return (
     <>
+    {/* Formulario de login: */}
+    {/* El onsubmit permite enviar la información. Handlesubmit se utiliza por convención para llamar a esta función.  */}
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
             value={email}
+            // El onChange actualiza los parámetros del UseState. Es una función con un evento. 
+            // El setEmail sirve para actualizar: le pasamos el evento(e) a escuchar y un valor (que es igual a email)
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Enter email"
           />
