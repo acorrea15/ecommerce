85dec6fca88d56e3ecadd4a3874861ec7bd08d18
adolfo85 <adolfodeboeck@gmail.com>
2022-12-21 10:26:21 -0300

    trabajo en el index.js con el context

--- src/components/context/UserProvider.jsx
+++ src/components/context/UserProvider.jsx
@@ -11,8 +11,8 @@ const UserProvider = ({children}) => {
 
     return (
 
-        // El user context sirve para pasar los componentes.
-        <userContext.Provider>
+        // El provider es el proveedor. Aquí ponemos los datos que queremos pasar. En este caso el user y la función login usuario.  
+        <userContext.Provider value={{user, loginUsuario}}>
             {children}
         </userContext.Provider>
     )
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import UserProvider from "./components/context/UserProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // El browserRouter va siempre en el index.js para comenzar a utilizar las rutas. 
   // Además, se crea el componente rutas y ese componente va en el app.js
   <BrowserRouter>
+  <UserProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+  </UserProvider>
   </BrowserRouter>
 );
 reportWebVitals();
