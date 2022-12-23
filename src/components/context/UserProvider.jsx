7116640895cc28963141a2aef931d445bd609636
adolfo85 <adolfodeboeck@gmail.com>
2022-12-23 11:47:44 -0300

    local Storage para almacenamiento de usuarios

--- src/components/context/UserProvider.jsx
+++ src/components/context/UserProvider.jsx
@@ -1,38 +1,36 @@
-import React, {createContext, useState} from 'react'
-// createContext es un hook de react que importamos. Guardamos todo en userContext (va a tener todos nuestros datos) 
+import React, { createContext, useState } from "react";
+// createContext es un hook de react que importamos. Guardamos todo en userContext (va a tener todos nuestros datos)
 // Esos datos se exportan para importarse posteriormente dentro del return
-export const userContext = createContext()
+export const userContext = createContext();
 
+// Este archivo es el proveedor de datos. En este caso los datos de usuario.
+// La función UserProvider es nuestro componente que posteriormente exportamos al final del código.
+// Traemos children: son props: datos que yo quiero disponibilizar en otros componentes.
+const UserProvider = ({ children }) => {
+  // creación de un state para los usuarios:
+  const [user, setUser] = useState("");
 
-
-// Este archivo es el proveedor de datos. En este caso los datos de usuario. 
-// La función UserProvider es nuestro componente que posteriormente exportamos al final del código. 
-// Traemos children: son props: datos que yo quiero disponibilizar en otros componentes. 
-const UserProvider = ({children}) => {
-
-    // creación de un state para los usuarios:
-    const [user, setUser] = useState("")
-
-    const loginUsuario = (email, password) => {
-    //    Creación de un objeto con los datos de usuario. 
+  const loginUsuario = (email, password) => {
+    //    Creación de un objeto con los datos de usuario. Función que permite guardar los datos del usuario. Estos datos se guardan
+    // En el SetUser
     const registroUser = {
-        email,
-        password,
-    }
-    setUser (registroUser)
-    
-    }
+      email,
+      password,
+    };
+    setUser(registroUser);
+    // Creación de local storage para almacenar los usuarios porque éstos no están almacenados en una base de datos: 
+    localStorage.setItem("registroUser"), JSON.stringify(registroUser)
+  };
 
-    return (
+  return (
+    // El provider es el proveedor. Aquí ponemos los datos que queremos pasar. Disponibilizamos los children dentro
+    // del componente userContext.Provider. No hace falta poner props x props (usuario, pass, etc) con poner children pasa todos los datos.
+    // Dentro de la etiqueta value van los datos que compartimos/disponibilizamos: en este caso los usuarios y la función loginUsuario
+    // Con lo siguiente decimos: userContext es nuestro proveedor de datos.
+    <userContext.Provider value={{ user, loginUsuario }}>
+      {children}
+    </userContext.Provider>
+  );
+};
 
-        // El provider es el proveedor. Aquí ponemos los datos que queremos pasar. Disponibilizamos los children dentro 
-        // del componente userContext.Provider. No hace falta poner props x props (usuario, pass, etc) con poner children pasa todos los datos. 
-        // Dentro de la etiqueta value van los datos que compartimos/disponibilizamos: en este caso los usuarios y la función loginUsuario
-        // Con lo siguiente decimos: userContext es nuestro proveedor de datos.   
-        <userContext.Provider value={{user, loginUsuario}} >
-            {children}
-        </userContext.Provider>
-    )
-}
-
-export default UserProvider
+export default UserProvider;
--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -1,17 +1,18 @@
-import React, {useState} from "react";
-import {Form, Button} from 'react-bootstrap'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 import { useForm } from "react-hook-form";
-import InputGroup from 'react-bootstrap/InputGroup';
+import InputGroup from "react-bootstrap/InputGroup";
 
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-const schema = yup.object({
-  firstName: yup.string().required(),
-  age: yup.number().positive().integer().required(),
-}).required();
-
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required();
 
 const Registro = () => {
   // Establecemos el estado inicial de nombre, email, etc. con el UseState
@@ -21,42 +22,59 @@ const Registro = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    setValue,
+  } = useForm();
 
-    const { register, formState: { errors }, handleSubmit, reset, setValue } = useForm();
- 
-    
-    const onSubmit = (data) => {
-      console.log(data, "<-----data");      
-      console.log(data.nombre, data.apellido, data.email, data.password, data.confirmPassword, "<-----data.Datos"); 
-      
-      /* setValue("nombre", "Probando SetValue...") */
-      console.log(name, lastName, email, password, confirmPassword, "<-----DatosssssName");
-      if(data.password === data.confirmPassword){
-        Swal.fire({
-          icon: "success",
-          title: "Registrado!",
-          text: "Registro exitoso!",
-        });
-        reset();
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "Las contraseñas no coinciden",
-          text: "Error de confirmación de contraseñas. Verifique las contraseñas ingresadas.",
-        });
-      }
-      
+  const onSubmit = (data) => {
+    console.log(data, "<-----data");
+    console.log(
+      data.nombre,
+      data.apellido,
+      data.email,
+      data.password,
+      data.confirmPassword,
+      "<-----data.Datos"
+    );
+
+    /* setValue("nombre", "Probando SetValue...") */
+    console.log(
+      name,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      "<-----DatosssssName"
+    );
+    if (data.password === data.confirmPassword) {
+      Swal.fire({
+        icon: "success",
+        title: "Registrado!",
+        text: "Registro exitoso!",
+      });
+      reset();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Las contraseñas no coinciden",
+        text: "Error de confirmación de contraseñas. Verifique las contraseñas ingresadas.",
+      });
     }
-  
+  };
+
   // const handleSubmit = (e) => {
   //   e.preventDefault();
-  //   console.log(email, password, confirmPassword);   
+  //   console.log(email, password, confirmPassword);
   //   if(password === confirmPassword){
   //     Swal.fire({
   //       icon: "success",
   //       title: "Registrado!",
   //       text: "Registro exitoso",
-  //     });      
+  //     });
   //   } else {
   //     Swal.fire({
   //       icon: "error",
@@ -69,123 +87,159 @@ const Registro = () => {
 
   return (
     <>
-        <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mt-4">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control
-              type="text"
-              /* value={name} */
-              /* onChange={ (e) => {setName(e.target.value); console.log(name, "<-----name");}   }  */
-              placeholder="Ingrese su nombre"
-              {...register("nombre", {  required: {value: true, 
-                                                  message: "El nombre es requerido"}, 
-                                        minLength: {value: 2,
-                                                    message: "El nombre debe tener al menos 2 caracteres"},
-                                        maxLength: {value: 30,
-                                                    message: "El nombre debe tener como máximo 30 caracteres"},            
-                                      pattern:{value: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/i ,
-                                                message: "El formato del nombre no es correcto"} 
-                                    }) }
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            type="text"
+            // Primera forma de hacer la función y redefinir el value y la actualización del useState
+            /* value={name} */
+            /* onChange={ (e) => {setName(e.target.value); console.log(name, "<-----name");}   }  */
+            placeholder="Ingrese su nombre"
+            // Validaciones en los diferentes campos:
+            {...register("nombre", {
+              required: { value: true, message: "El nombre es requerido" },
+              minLength: {
+                value: 2,
+                message: "El nombre debe tener al menos 2 caracteres",
+              },
+              maxLength: {
+                value: 30,
+                message: "El nombre debe tener como máximo 30 caracteres",
+              },
+              pattern: {
+                value: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/i,
+                message: "El formato del nombre no es correcto",
+              },
+            })}
+            // Segunda forma de hacer la Función que envía los datos al setName y actualiza el valor del UserState
+            // El Onchange toma el evento y lo guarda en setName
+            onChange={(e) => {
+              setName(e.target.value);
+              console.log(name, "<-----name");
+            }}
+          />
+          {/* Estilo del mensaje de error: */}
+          <div className="text-danger">{errors.nombre?.message}</div>
 
-            onChange={ (e) => {setName(e.target.value); console.log(name, "<-----name");}   }                        
-            />
-          {/* {errors.nombre && <span>{errors.nombre.message}</span>} */}
-          <div className="text-danger" >{errors.nombre?.message}</div>
-
-            <Form.Group className="mt-4">
+          <Form.Group className="mt-4">
             <Form.Label>Apellido</Form.Label>
             <Form.Control
               type="text"
               /* value={lastName} */
-              
+
               placeholder="Ingrese su Apellido"
-              {...register("apellido", {  required: {value: true, 
-                                                  message: "El apellido es requerido"}, 
-                                        minLength: {value: 2,
-                                                    message: "El apellido debe tener al menos 2 caracteres"},
-                                        maxLength: {value: 30,
-                                                    message: "El apellido debe tener como máximo 30 caracteres"},            
-                                      pattern:{value: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/i ,
-                                                message: "El formato del apellido no es correcto"} 
-                                    }) }
-             onChange={(e) => setLastName(e.target.value)}                       
+              {...register("apellido", {
+                required: { value: true, message: "El apellido es requerido" },
+                minLength: {
+                  value: 2,
+                  message: "El apellido debe tener al menos 2 caracteres",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "El apellido debe tener como máximo 30 caracteres",
+                },
+                pattern: {
+                  value: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/i,
+                  message: "El formato del apellido no es correcto",
+                },
+              })}
+              onChange={(e) => setLastName(e.target.value)}
             />
             {/* {errors.apellido && <span>{errors.apellido.message}</span>} */}
-            <div className="text-danger" >{errors.apellido?.message}</div>                        
+            <div className="text-danger">{errors.apellido?.message}</div>
           </Form.Group>
-          </Form.Group>
-        
-          <Form.Group className="mt-4">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              /* value={email} */
-              
-              placeholder="Ingrese su email"
-              {...register("email", { required: {value: true, 
-                                                message: "El email es requerido"}, 
-                                      pattern:{value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i ,
-                                              message: "El formato del email no es correcto"} 
-                                    }) }
-            onChange={(e) => setEmail(e.target.value)}                        
-            />
-            {/* {errors.email && <span>{errors.email.message}</span>} */}
-            <div className="text-danger" >{errors.email?.message}</div>  
-          </Form.Group>
+        </Form.Group>
 
-        
-          <Form.Group className="mt-4">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              /* value={password} */
-              
-              placeholder="Ingresar contraseña"
-              {...register("password", { required: {value: true, 
-                                                    message: "La contraseña es requerida"}, 
-                                        minLength: {value: 8,
-                                                    message: "La contraseña debe tener al menos 8 caracteres"},
-                                        maxLength: {value: 64,
-                                                      message: "La contraseña debe tener como máximo 64 caracteres"},            
-                                        pattern:{value: /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/i ,
-                                                  message: "La contraseña debe tener una letra minúscula, una letra mayúscula, un número, un carácter especial y mínimo 8 dígitos."}            
-                                        })}
-             onChange={(e) => setPassword(e.target.value)}
-            />
-            {/* {errors.password && <span>{errors.password.message}</span>} */}
-            <div className="text-danger" >{errors.password?.message}</div>  
-          </Form.Group>
-          
-          <Form.Group className="mt-4">
-            <Form.Label>Confirme Password</Form.Label>
-            <Form.Control
-              type="password"
-              /* value={confirmPassword} */
-              
-              placeholder="Confirme su password"
-              {...register("confirmPassword", { required: {value: true, 
-                                                          message: "La confirmación de la contraseña es requerida"}, 
-                                                minLength: {value: 8,
-                                                            message: "La contraseña debe tener al menos 8 caracteres"},
-                                                maxLength: {value: 64,
-                                                              message: "La contraseña debe tener como máximo 64 caracteres"},            
-                                                pattern:{value: /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/i ,
-                                                          message: "La contraseña debe tener una letra minúscula, una letra mayúscula, un número, un carácter especial y mínimo 8 dígitos."}            
-                                                })}    
-              onChange={(e) => setConfirmPassword(e.target.value)}                                           
-            />
-            {/* {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>} */}
-            <div className="text-danger" >{errors.confirmPassword?.message}</div>                                     
-          </Form.Group>
-          <Button variant="primary" type="submit" className="mt-4">
-            Submit
-          </Button>
-        </Form>
+        <Form.Group className="mt-4">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            /* value={email} */
+
+            placeholder="Ingrese su email"
+            {...register("email", {
+              required: { value: true, message: "El email es requerido" },
+              pattern: {
+                value:
+                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i,
+                message: "El formato del email no es correcto",
+              },
+            })}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* {errors.email && <span>{errors.email.message}</span>} */}
+          <div className="text-danger">{errors.email?.message}</div>
+        </Form.Group>
+
+        <Form.Group className="mt-4">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            /* value={password} */
+
+            placeholder="Ingresar contraseña"
+            {...register("password", {
+              required: { value: true, message: "La contraseña es requerida" },
+              minLength: {
+                value: 8,
+                message: "La contraseña debe tener al menos 8 caracteres",
+              },
+              maxLength: {
+                value: 64,
+                message: "La contraseña debe tener como máximo 64 caracteres",
+              },
+              pattern: {
+                value:
+                  /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/i,
+                message:
+                  "La contraseña debe tener una letra minúscula, una letra mayúscula, un número, un carácter especial y mínimo 8 dígitos.",
+              },
+            })}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* {errors.password && <span>{errors.password.message}</span>} */}
+          <div className="text-danger">{errors.password?.message}</div>
+        </Form.Group>
+
+        <Form.Group className="mt-4">
+          <Form.Label>Confirme Password</Form.Label>
+          <Form.Control
+            type="password"
+            /* value={confirmPassword} */
+
+            placeholder="Confirme su password"
+            {...register("confirmPassword", {
+              required: {
+                value: true,
+                message: "La confirmación de la contraseña es requerida",
+              },
+              minLength: {
+                value: 8,
+                message: "La contraseña debe tener al menos 8 caracteres",
+              },
+              maxLength: {
+                value: 64,
+                message: "La contraseña debe tener como máximo 64 caracteres",
+              },
+              pattern: {
+                value:
+                  /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/i,
+                message:
+                  "La contraseña debe tener una letra minúscula, una letra mayúscula, un número, un carácter especial y mínimo 8 dígitos.",
+              },
+            })}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {/* {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>} */}
+          <div className="text-danger">{errors.confirmPassword?.message}</div>
+        </Form.Group>
+        <Button variant="primary" type="submit" className="mt-4">
+          Submit
+        </Button>
+      </Form>
     </>
   );
 };
 
 export default Registro;
- 
-
- 
